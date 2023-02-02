1c4a85e8a4ce5717deaf65065dd1e35cc2710628
konstantin <konstantindeiker@googlemail.com>
2023-02-02 01:03:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// const form = document.querySelector('.form');
+// const title = document.querySelector('#title');
+// const price = document.querySelector('#price');
+// const sectionProducts = document.querySelector('#products');
+
+
+// const products = [];
+// form.onsubmit = (e) => {
+//     e.preventDefault(); // запрет на переход по (ссылке)
+//     products.push({
+//         title: title.value,
+//         price: price.value,
+//     })
+
+//     newProducts();
+
+//     title.value = '';
+//     price.value = '';
+
+// }
+
+// function createNode(title, price) {
+//     let div = document.createElement('div');
+//     div.classList.add('product');
+
+//     let titleP = document.createElement('p');
+//     let priceP = document.createElement('p');
+
+//     titleP.innerText = title;
+//     priceP.innerText = price;
+
+//     div.append(titleP, priceP);
+//     sectionProducts.append(div);
+
+// }
+
+// function newProducts() {
+//     sectionProducts.innerHTML = '';
+//     for(let i = 0; i < products.length; i++){
+//         createNode(products[i].title, products[i].price);
+//     }
+// }
+
+// const totalPrice = document.querySelector('.btn2');
+
+const passwordBtn = document.querySelector('.btn3');
+const form = document.querySelector('.form');
+
+let password = [];
+
+for(let i = 0; i < password.length; i++){
+    
+}
+
+
+// Что то до меня совсем не доходит что к чему, буду переспатривать последние лецкии, 
+// может изза усталости совсем ничего незапоминаю, последние недели много работы и впридачу ещё заболел(
